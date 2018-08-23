8bde54571ef2a0030110fb3383ff7b13a3dfc237
Aleksey Abramov <capiton2008@gmail.com>
2018-08-23 10:23:15 +0300

    Создал класс вектор

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
 'use strict';
+
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vector) {
+    if (typeof vector != 'object' && vector.x === undefined && vector.y === undefined) {
+      throw new Error('Можно прибавлять к вектору только вектор типа Vector')
+    }
+    return new Vector(this.x + vector.x, this.y + vector.y);
+  }
+
+  times(count) {
+    return new Vector(count * this.x, count * this.y);
+  }
+}
