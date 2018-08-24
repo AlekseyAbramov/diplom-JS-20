a0446256a29c34418750d22853b895259e0e751a
Aleksey Abramov <capiton2008@gmail.com>
2018-08-24 23:24:16 +0300

    Vector готов. В Actor 2 ошибки в тестах.

--- game.js
+++ game.js
@@ -7,8 +7,8 @@ class Vector {
   }
 
   plus(vector) {
-    if (typeof vector != 'object' && vector.x === undefined && vector.y === undefined) {
-      throw new Error('Можно прибавлять к вектору только вектор типа Vector')
+    if (isNotVector(vector)) {
+      throw new Error('Можно прибавлять к вектору только вектор типа Vector');
     }
     return new Vector(this.x + vector.x, this.y + vector.y);
   }
@@ -18,21 +18,18 @@ class Vector {
   }
 }
 
-function isVector(vector) {
-  if (typeof vector === 'object' && vector.x != undefined && vector.y != undefined) {
-    return true;
-  }
+function isNotVector(vector) {
+  return (typeof vector != 'object' || !('x' in vector) || !('y' in vector) || !('plus' in vector) || !('times' in vector))
 }
 
 class Actor {
   constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-    if (isVector(pos) && isVector(size) && isVector(speed)) {
-      this.pos = pos;
-      this.size = size;
-      this.speed = speed;
-    } else {
-      throw new Error('Класс Actor можно создавать только векторами типа Vector')
+    if (isNotVector(pos) || isNotVector(size) || isNotVector(speed)) {
+      throw new Error('Класс Actor можно создавать только векторами типа Vector');
     }
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
     this.act = function() {};
     Object.defineProperty(this, 'left', {
       get: function() {
@@ -41,7 +38,7 @@ class Actor {
     });
     Object.defineProperty(this, 'top', {
       get: function() {
-        return this.pos.y + this.size.y;
+        return this.pos.y;
       }
     });
     Object.defineProperty(this, 'right', {
@@ -51,22 +48,21 @@ class Actor {
     });
     Object.defineProperty(this, 'bottom', {
       get: function() {
-        return this.pos.y;
+        return this.pos.y + this.size.y;
       }
     });
     Object.defineProperty(this, 'type', {
-      value: function() {
-        return 'actor';
+      value: 'actor'
       }
-    });
+    );
   }
 
   isIntersect(actor) {
     if(this === actor) {
       return false;
-    } else if (actor.type() != 'actor') {
+    } else if (actor.type != 'actor') {
         throw new Error('Можно сравнивать только объекты класса Actor');
     }
-    return ( this.top <= actor.bottom || this.bottom >= actor.top || this.right <= actor.left || this.left >= actor.right );
+    return ( !(this.top < actor.bottom || this.bottom > actor.top || this.right < actor.left || this.left > actor.right) );
   }
 }
