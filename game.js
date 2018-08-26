3853cad606b343e09984ca416ec4a784a2ee50bd
Aleksey Abramov <capiton2008@gmail.com>
2018-08-26 21:48:54 +0300

    Исправления в Actor и часть Level

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ class Vector {
   }
 
   plus(vector) {
-    if (isNotVector(vector)) {
+    if (!(vector instanceof Vector)) {
       throw new Error('Можно прибавлять к вектору только вектор типа Vector');
     }
     return new Vector(this.x + vector.x, this.y + vector.y);
@@ -18,13 +18,9 @@ class Vector {
   }
 }
 
-function isNotVector(vector) {
-  return (typeof vector != 'object' || !('x' in vector) || !('y' in vector) || !('plus' in vector) || !('times' in vector))
-}
-
 class Actor {
   constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-    if (isNotVector(pos) || isNotVector(size) || isNotVector(speed)) {
+    if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
       throw new Error('Класс Actor можно создавать только векторами типа Vector');
     }
     this.pos = pos;
@@ -51,18 +47,78 @@ class Actor {
         return this.pos.y + this.size.y;
       }
     });
-    Object.defineProperty(this, 'type', {
-      value: 'actor'
+    /*Object.defineProperty(this, 'type', {
+      get: function() {return 'actor'},
+      enumerable: true,
+      configurable: true,
       }
-    );
+    );*/
+  }
+
+  get type() {
+    return 'actor';
   }
 
   isIntersect(actor) {
     if(this === actor) {
       return false;
-    } else if (actor.type != 'actor') {
+    } else if (!(actor instanceof Actor)) {
         throw new Error('Можно сравнивать только объекты класса Actor');
     }
-    return ( !(this.top < actor.bottom || this.bottom > actor.top || this.right < actor.left || this.left > actor.right) );
+    //return ( !(this.top < actor.bottom || this.bottom > actor.top || this.right < actor.left || this.left > actor.right) );
+    return ( !(this.left >= actor.left + actor.size.x || actor.left >= this.left + this.size.x || this.top >= actor.top + actor.size.y || actor.top >= this.top + this.size.y) );
+  }
+}
+
+class Level {
+  constructor(arrGrid = [], arrActors = []) {
+    this.grid = arrGrid;
+    this.actors = arrActors.filter(function(el) {
+      return ('type' in el);
+    });
+    this.player = this.actors.find(actor => actor.type === 'player');
+    /*this.player = arrActors.find(function(actor) {
+      return actor.type == 'player';
+    });*/
+    this.height = this.grid.length;
+    this.width = this.grid.reduce(function(memo, el) {
+      if (el.length > memo) {
+        memo = el.length;
+      }
+      return memo;
+    }, 0);
+    this.status = null;
+    this.finishDelay = 1;
+  }
+
+  isFinished() {
+    return (this.status != null && this.finishDelay < 0)
+  }
+
+  actorAt(actor = {}) {
+    if (!(actor instanceof Actor)) {
+      throw new Error('Можно сравнивать только объекты класса Actor');
+    }
+    return this.actors.find(function(el) {
+      return el.isIntersect(actor);
+    });
+  }
+
+  obstacleAt(pos, size) {
+    if (!(pos instanceof Vector) || !(size instanceof Vector)) {
+      throw new Error('В метод obstacleAt можно передавать только вектора типа Vector');
+    }
+    let obstacleActor = new Actor(pos, size);
+    let left = this.grid.find(function(el) {
+      return el.length == this.width;
+    });
+    if(obstacleActor.top > this.grid.indexOf() ) {
+      return 'wall';
+    }
+    if (this.actorAt(obstacleActor)) {
+      return this.grid[pos.y][pos.x];
+    } else {
+      return undefined;
+    }
   }
 }
